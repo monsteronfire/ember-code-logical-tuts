b50dfc4b3c949aa878119fabc635989a17f739e0
monsteronfire <monsteronfire@gmail.com>
2016-04-26 08:45:41 +0800

    Add practice for Ember getters and setters

--- intro/app/controllers/first.js
+++ intro/app/controllers/first.js
@@ -1,5 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  firstName: 'Shaii'
+  firstName: 'Johnny',
+  lastName: 'Depp',
+  actions: {
+    showAlert: function() {
+      // Getting properties of controller inside a method
+      // 'this' keyword here refers to the controller
+
+      this.set('firstName', 'Johnny');
+      this.set('lastName', 'Depp');
+
+      var actor = this.get('firstName') + ' ' + this.get('lastName');
+
+      alert('The actor is ' + actor);
+    },
+    setAnotherActor: function() {
+      // .set() takes two arguments. first is the property to set. second argument is the value to set it to
+      this.set('firstName', 'Orlando');
+      this.set('lastName', 'Bloom');
+
+      var newActor = this.get('firstName') + ' ' + this.get('lastName');
+
+      alert('The other actor is ' + newActor);
+    }
+  }
 });
