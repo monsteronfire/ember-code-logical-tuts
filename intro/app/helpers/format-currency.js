aa14f4dc1b40387688308bd5b7e09b9e45d573ee
monsteronfire <monsteronfire@gmail.com>
2016-04-28 09:23:44 +0800

    Implement a helper

---
+++ intro/app/helpers/format-currency.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export function formatCurrency(params/*, hash*/) {
+  let value = params[0],
+      dollars = Math.floor(value/100),
+      cents = value % 100,
+      sign = '$';
+
+  if ( cents.toString().length === 1 ) {
+    cents = '0' + cents;
+  }
+
+  return `${sign}${dollars}.${cents}`;
+}
+
+export default Ember.Helper.helper(formatCurrency);
